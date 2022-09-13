a5cfcfe44a388e4f5cfc66ddf5ca159dfe198ef5
kadelcode <kadelcode@gmail.com>
2022-09-13 18:36:19 +0100

    Adding login and logout functionality

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -126,3 +126,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'posts'
+
+LOGOUT_REDIRECT_URL = 'posts'
